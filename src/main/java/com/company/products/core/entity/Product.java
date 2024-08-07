9dc62fde97787855c06a403e383bfef04ac3c40a
Hamza Sallam <hamza.sallam.eng@outlook.com>
2024-08-07 23:02:14 +0300

    added new layers for more concerns separation

--- src/main/java/com/company/products/core/entity/Product.java
+++ src/main/java/com/company/products/core/entity/Product.java
@@ -2,16 +2,16 @@ package com.company.products.core.entity;
 
 
 public class Product {
-     Long id;
-     String name;
-     String description;
-     double price;
-     String image;
+     private Long id;
+     private String name;
+     private String description;
+     private double price;
+     private String image;
 
 
     // Getters and Setters
     public Long getId() {return id;}
-
+    public void setId(Long id) {this.id = id;}
     public String getName() {return name;}
     public void setName(String name) {this.name = name;}
 
